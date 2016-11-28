c4f7bcc2b47289ccd54819b6799ed5bbec4a7571
shreiya chowdhary <shreiya.c@gmail.com>
2016-11-29 01:07:37 +0530

    everything's working

---
+++ src/SecondChild.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class SecondChild extends Component{
+
+  render(){
+    return(
+      <section className="module content">
+<div className="container">
+  <p>HI</p>
+
+</div>
+</section>
+    )
+  }
+}
+
+export default SecondChild;
